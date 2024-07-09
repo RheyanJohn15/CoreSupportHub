2414ab229658f321d0da4658be1e405c529716bd
Rheyan <142854437+RheyanJohn15@users.noreply.github.com>
2024-07-09 15:13:00 +0800

    Testing things

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+'use client';
+
+const Button = ({text}) => {
+ return <button className="bg-accent p-3 rounded-full">{text}</button>;
+}
+
+export default Button;
---
+++ src/strings/primary.js
@@ -0,0 +1,15 @@
+const Text = {
+  AppName: 'Core Support Hub',
+
+  //Navigation
+  Nav: {
+    home: "Home",
+    service: "Services",
+    learn: "Learn More",
+    contact: "Contact Us",
+    industries: "Industries",
+    why: "Why Core Support Hub",
+  }
+}
+
+export default Text;
