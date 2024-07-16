4bedeb2496a8172957b6332bf7391a3cc96c4a8a
Rheyan <142854437+RheyanJohn15@users.noreply.github.com>
2024-07-16 18:12:48 +0800

    Initial Landing Page Update

--- src/Components/LandingBlock.js
+++ src/Components/LandingBlock.js
@@ -1,6 +1,17 @@
+'use client';
+
+import String from '@/Strings/english';
+import Image from 'next/image';
+import Logo from '../../public/logo/logo2.png';
 const LandingBlock = () => {
    return (
-     <main className="w-full h-screen"></main>
+     <main className="w-full h-screen flex flex-col justify-center items-center">
+     <div className="flex flex-col justify-end items-center w-full h-1/2">
+     <Image src={Logo} alt='Logo' width={100} />
+     <h1 className='text-6xl font-semibold mb-8 text-main'>{String.App}</h1>
+     </div>
+      <div className='flex flex-col justify-start items-center w-full h-1/2'> <h1 className='text-4xl mt-8 text-white'>{String.Tag1}</h1></div>
+     </main>
    );
 }
 export default LandingBlock;
