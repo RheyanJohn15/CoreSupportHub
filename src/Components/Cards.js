778a62e880843c8a72178e21ebb5c6374a78b438
JpUbas <162091712+JpUbas@users.noreply.github.com>
2024-07-16 17:48:35 +0800

    update cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef, useState } from "react";
 import {
@@ -8,7 +10,7 @@ import {
   Button,
 } from "@material-tailwind/react";
 import React from 'react';
-import { FaMicrochip, FaHeadset, FaRegLightbulb, FaCircleArrowDown } from "react-icons/fa6";
+import { FaMicrochip, FaHeadset, FaRegLightbulb } from "react-icons/fa6";
 
 const Example = () => {
   return (
@@ -26,8 +28,7 @@ const HorizontalScrollCarousel = () => {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["15%", "-40%"]);
-  const xSmallScreen = useTransform(scrollYProgress, [0, 1], ["5%", "-92%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["5%", "-92%"]);
 
   return (
     <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
@@ -35,19 +36,10 @@ const HorizontalScrollCarousel = () => {
         <h1 className="pl-5 text-4xl text-quicksand text-main md:text-5xl lg:text-6xl whitespace-nowrap">
           Our Services
         </h1>
-        <motion.div
-          style={{ x: window.innerWidth < 768 ? xSmallScreen : x }}
-          className="flex gap-20"
-        >
+        <motion.div style={{ x }} className="flex gap-20">
           {cards.map((card) => {
             return <CardComponent card={card} key={card.id} />;
           })}
-          <div className="flex items-center justify-center mt-16 rounded-xl h-40 w-80 md:w-96 bg-white">
-            <h1 className="text-4xl text-quicksand text-main md:text-5xl lg:text-6xl whitespace-nowrap">
-              Explore More
-            </h1>
-            <FaCircleArrowDown size={40} className="ml-1 text-main mt-4" />
-          </div>
         </motion.div>
       </div>
     </section>
